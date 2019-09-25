5a31667b8790ac69d458fe13e9c5317b7757dbdf
yari icke <yari.icke@gmail.com>
2019-09-25 11:43:17 +0200

    Post Processing and render pipeline

--- Mobile_Game_P9/Assets/Yari/Scripts/Accelerometer.cs
+++ Mobile_Game_P9/Assets/Yari/Scripts/Accelerometer.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class Accelerometer : MonoBehaviour
 {
+    public CameraShake cameraShake;
     private Rigidbody rb;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float shakeSensitivty = 1f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -13,7 +19,28 @@ public class Accelerometer : MonoBehaviour
 
     void Update()
     {
-        rb.AddForce(Input.acceleration, ForceMode.Acceleration);
+        Vector3 tilt = Input.acceleration;
+
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;
+
+        rb.AddForce(tilt, ForceMode.Acceleration);
+
+        if (tilt.x > shakeSensitivty || tilt.x < shakeSensitivty * -1f)
+        {
+            text.text = "SHAKE X";
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+        }
+
+        if (tilt.z > shakeSensitivty || tilt.z < shakeSensitivty * -1f)
+        {
+            text.text = "SHAKE Z";
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+        }
     }
 
     public void Restart()
