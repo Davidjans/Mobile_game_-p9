653ccf6ad81f80ca5695f0fd44b801717ba4ab47
yari icke <yari.icke@gmail.com>
2019-09-25 09:14:33 +0200

    added my folder and accelerometer test

---
+++ Mobile_Game_P9/Assets/Yari/Scripts/Accelerometer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.AddForce(Input.acceleration, ForceMode.Acceleration);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("AccelerometerTestScene");
+    }
+}
