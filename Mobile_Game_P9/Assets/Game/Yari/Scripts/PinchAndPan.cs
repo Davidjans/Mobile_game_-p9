c3cf64e898d8689165c04143ba5b075fe5101d16
yari icke <yari.icke@gmail.com>
2019-10-02 11:44:15 +0200

    added tilt camera controls

---
+++ Mobile_Game_P9/Assets/Game/Yari/Scripts/PinchAndPan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchAndPan : MonoBehaviour
+{
+    private Vector3 touchStart;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Input.mousePosition;
+            touchStart.z = Camera.main.nearClipPlane;
+            touchStart = Camera.main.ScreenToWorldPoint(touchStart);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = Input.mousePosition;
+            touchStart.z = Camera.main.nearClipPlane;
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+    }
+}
---
+++ Mobile_Game_P9/Assets/Game/Yari/Scripts/ViewTilt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewTilt : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;        
+
+        Quaternion startRotation = gameObject.transform.localRotation;
+
+        Quaternion cameraTilt = Quaternion.Euler(startRotation.x += (tilt.y * 2f), startRotation.y += (tilt.x * 2f), startRotation.z);
+
+        gameObject.transform.localRotation = Quaternion.Slerp(gameObject.transform.localRotation, cameraTilt, 0.3f);
+    }
+}
